47e85351d2e064dae944902e46f246e55f025f51
Pollyana <pollyanarocha416@gmail.com>
2022-05-29 18:05:01 -0300

    Adicionar arquivos de projeto.

---
+++ ConstrutorOOP/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrutorOOP
+{
+    class Pessoa
+    {
+        // Construtor
+        public Pessoa()
+        {
+            Console.WriteLine("Construtor executado");
+        }
+
+        public Pessoa(string nome)
+        {
+            Console.WriteLine("Olá " + nome);
+        }
+        public Pessoa(int idade)
+        {
+            Console.WriteLine("idade " + idade);
+        }
+
+        public Pessoa(double cpf)
+        {
+            Console.WriteLine("cpf " + cpf);
+        }
+
+    }
+}
